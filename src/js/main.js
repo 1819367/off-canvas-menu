4417cbe01d5095ca20ce1ba9125f0636dbcfc968
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-07 17:10:11 -0700

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Start writing JavaScript here!
+// Select the button
+const button = document.querySelector('button')
+
+// add the event listener
+button.addEventListener('click', event => {
+  console.log('push the screen!')
+})
